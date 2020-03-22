3c5e15797056323da273b2b3cc4e6180ea21dc57
Thomas Nguyen <tnguyenswe@gmail.com>
2020-03-21 17:58:54 -0700

    Update to JustJava Application

--- JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -2,16 +2,23 @@ package com.example.android.justjava;
 
 import androidx.appcompat.app.AppCompatActivity;
 import java.text.NumberFormat;
+
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * This app displays an order form to order coffee.
  */
 public class MainActivity extends AppCompatActivity {
 
-    int quantity = 0;
+    int quantity = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,17 +30,47 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int price = calculatePrice();
-        displayMessage(orderSummary(calculatePrice()));
+        CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.whipped_cream_check_box);
+        CheckBox chocolateCheckBox = (CheckBox) findViewById(R.id.chocolate_checkbox);
+
+        EditText name_value = (EditText) findViewById(R.id.name_field);
+        String name = name_value.getText().toString();
+        Log.v("MainActivity", "Name: " + name);
+        Log.v("MainActivity", "Has whipped cream is: " + whippedCreamCheckBox.isChecked());
+        boolean hasWhippedCream = whippedCreamCheckBox.isChecked();
+        boolean hasChocolate = chocolateCheckBox.isChecked();
+        int price = calculatePrice(hasWhippedCream, hasChocolate);
+        String priceMessage = createOrderSummary(name, price,hasWhippedCream, hasChocolate);
+        //displayMessage(priceMessage);
+
+        //You can comment the next few intent lines and uncomment the displayMessage line
+        //if you just want it to show up on the app instead of using Gmail.
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(intent.EXTRA_SUBJECT, "Just Java order for " + name);
+        intent.putExtra(intent.EXTRA_TEXT, priceMessage);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 
     public void increment(View view) {
-        quantity+=1;
-        displayQuantity(quantity);
+        if(quantity<100) {
+            quantity += 1;
+            displayQuantity(quantity);
+        }
+        else{
+            Toast.makeText(this, "You can't have more than 100 coffees", Toast.LENGTH_SHORT).show();
+        }
     }
     public void decrement(View view) {
-        quantity-=1;
-        displayQuantity(quantity);
+        if(quantity>1) {
+            quantity -= 1;
+            displayQuantity(quantity);
+        }
+        else{
+            Toast.makeText(this, "You can't have less than 1 coffee", Toast.LENGTH_SHORT).show();
+        }
     }
 
     /**
@@ -41,16 +78,30 @@ public class MainActivity extends AppCompatActivity {
      *
      * @return the price
      */
-    private int calculatePrice() {
-        int price = quantity * 5;
-        return price;
+    private int calculatePrice(boolean addWhippedCream, boolean addChocolate) {
+        int basePrice = 5;
+
+        if(addWhippedCream){
+            basePrice = basePrice + 1;
+        }
+        if(addChocolate){
+            basePrice = basePrice + 2;
+        }
+
+        return quantity*basePrice;
     }
 
     /**
      * Gives a summary of the entire order.
      */
-    private String orderSummary(int number){
-        return("Name: Kaptain Kunal " + "\nQuantity: " + quantity + "\nTotal: $" + calculatePrice() + "\nThank you!");
+    private String createOrderSummary(String name, int price, boolean whippedCream, boolean chocolate){
+        String priceMessage = "Name: " + name;
+        priceMessage += "\nAdd whipped cream? " + whippedCream;
+        priceMessage += "\nAdd chocolate? " + chocolate;
+        priceMessage += "\nQuantity: " + quantity;
+        priceMessage += "\nTotal: $" + price;
+        priceMessage += "\nThank You!";
+        return priceMessage;
     }
     /**
      * This method displays the given quantity value on the screen.
